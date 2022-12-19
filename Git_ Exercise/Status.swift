062ea8ab318c9db7dd8a9452c10dbe44fcb2ba95
Sei Kim <vanism7557@gmail.com>
2022-12-19 11:23:21 +0900

    feat: Status - Result Message 추가

--- Git_ Exercise/Status.swift
+++ Git_ Exercise/Status.swift
@@ -42,3 +42,34 @@ extension Status {
         }
     }
 }
+
+//MARK: - Result Message
+
+extension Status {
+    func resMessage(input: ParsedInput) -> String? {
+        switch self {
+        case .addStudent:
+            if let name = input[0] as? String {
+                return Info.Student.added(name: name)
+            }
+        case .deleteStudent:
+            if let name = input[0] as? String {
+                return Info.Student.deleted(name: name)
+            }
+        case .addScore:
+            if let name = input[0] as? String,
+               let course = input[1] as? String,
+               let score = input[2] as? Score {
+                return Info.Score.added(name: name, course: course, score: score.description)
+            }
+        case .deleteScore:
+            if let name = input[0] as? String,
+               let course = input[1] as? String {
+                return Info.Score.deleted(name: name, course: course)
+            }
+        default:
+            return nil
+        }
+        return Info.Warning.failedToLoadReaction
+    }
+}
