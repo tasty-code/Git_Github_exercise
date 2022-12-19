fdcb06f5eaff282d45b24714d197cc8192b68b03
zhunhe <xuzhunhe@gmail.com>
2022-12-19 11:35:43 +0900

    feat: Implement addStudent()
    - Exit the program when you enter eof
    - Cannot be used as a name unless "" and ASCII code
    - Cannot be added if the name is duplicated

---
+++ Git_ Exercise/Command/Student.swift
@@ -0,0 +1,16 @@
+//
+//  Student.swift
+//  Git_ Exercise
+//
+//  Created by jun on 2022/12/19.
+//
+
+import Foundation
+
+struct Student {
+    let name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
--- Git_ Exercise/Command/System.swift
+++ Git_ Exercise/Command/System.swift
@@ -8,12 +8,32 @@
 import Foundation
 
 final class System {
+    private var students: [Student]
 
-    init() {}
-
+    init() {
+        self.students = []
+    }
     // 명렁어 - 학생추가 구현부
     func addStudent() -> State {
-        print("1. 학생추가")
+        // 설명 안내 문구 출력, 추가할 학생의 이름을 입력받음
+        // EOF(Ctrl + D)는 프로그램 종료로 처리
+        guard let studentName = getLine(messageType: .pleaseInputStudentNameToAdd) else {
+            return .quit
+        }
+        // 이름 유효성 검사
+        guard checkValidInput(studentName) else {
+            printMessage(messageType: .inputError)
+            return .continued
+        }
+        // 이미 존재하는 이름은 사용할 수 없음
+        if let index = findStudentIndex(name: studentName) {
+            print(self.students[index].name, terminator: "")
+            printMessage(messageType: .cannotAddStudentAlreadyExistName)
+            return .continued
+        }
+        self.students.append(Student(name: studentName))
+        print(studentName, terminator: " ")
+        printMessage(messageType: .addedStudent)
         return .continued
     }
 
@@ -44,5 +64,26 @@ final class System {
     // 명렁어 - 종료 구현부
     func quit() -> State {
         return .quit
+    }
+
+    // 이름으로 학생이 존재하는지 확인 후 인덱스 반환
+    func findStudentIndex(name: String) -> Int? {
+        for (index, student) in students.enumerated() where student.name == name {
+            return index
+        }
+        return nil
+    }
+
+    // 입력값 유효성 검사
+    func checkValidInput(_ name: String) -> Bool {
+        // ""은 유효하지 않은 입력
+        guard !name.isEmpty else {
+            return false
+        }
+        // 한글 및 다른 문자 체계는 이름으로 사용할 수 없음
+        guard name.filter({$0.isASCII && $0 != "\0"}).count == name.count else {
+            return false
+        }
+        return true
     }
 }
