f2231e671a23e2ec642734bd2d0000f843334357
devxsby <devxsby@gmail.com>
2022-12-19 11:14:23 +0900

    feat: create toDouble function in score enum

--- Git_ Exercise/enum/Score.swift
+++ Git_ Exercise/enum/Score.swift
@@ -17,4 +17,27 @@ enum Score: String {
     case dPlus = "D+"
     case dZero = "D0"
     case fail = "F"
+    
+    func toDouble() -> Double {
+        switch self {
+        case .aPlus:
+            return 4.5
+        case .aZero:
+            return 4.0
+        case .bPlus:
+            return 3.5
+        case .bZero:
+            return 3.0
+        case .cPlus:
+            return 2.5
+        case .cZero:
+            return 2.0
+        case .dPlus:
+            return 1.5
+        case .dZero:
+            return 1.0
+        case .fail:
+            return 0
+        }
+    }
 }
