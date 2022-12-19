3e818d3d99f651e5813ce5285b35a95166bef0f6
Jason <ggj00125@gmail.com>
2022-12-19 11:32:26 +0900

    Feat: 구현해야할 기능별로 메서드 초기 구현
    메서드 별로 구현할 예정인 기능
    - addToStudent(): 학생을 추가하는 기능을 구현
    - deleteToStudent(): 학생을 삭제하는 기능을 구현
    - addToChangeToGrade(): 학생의 성적을 추가하거나 변경할 기능을 구현
    - viewOfAverage(): 학생의 성적 평균을 계산하는 기능을 구현
    - exitMenu(): 프로그램을 종료하는 기능을 구현

---
+++ Git_ Exercise/Process.swift
@@ -0,0 +1,30 @@
+//
+//  Process.swift
+//  Git_ Exercise
+//
+//  Created by Jason on 2022/12/19.
+//
+
+import Foundation
+
+class Process {
+    func addToStudent() {
+        
+    }
+    
+    func deleteToStudent() {
+        
+    }
+    
+    func addOrChangeToGrade() {
+        
+    }
+    
+    func viewOfAverage() {
+        
+    }
+    
+    func exitMenu(input value: String?) {
+        
+    }
+}
