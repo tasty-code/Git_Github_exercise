7fa902b7d60871202a92128ca799dfb39a0f126a
devxsby <devxsby@gmail.com>
2022-12-19 11:09:48 +0900

    feat: add program menu enum types

---
+++ Git_ Exercise/enum/Menu.swift
@@ -0,0 +1,17 @@
+//
+//  Menu.swift
+//  Git_ Exercise
+//
+//  Created by devxsby on 2022/12/19.
+//
+
+import Foundation
+
+enum Menu: String {
+    case addStudent = "1"
+    case deleteStudent = "2"
+    case addOrChangeGrades = "3"
+    case deleteGrades = "4"
+    case showGrades = "5"
+    case exitProgram = "X"
+}
