0633bb61b9bdba247166035d30f338e2a12abe7c
esther <wjswls109@naver.com>
2022-12-19 11:31:12 +0900

    Feat. 학생 추가 기능 추가

--- Git_ Exercise/main.swift
+++ Git_ Exercise/main.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 var list: [Dictionary<String, String>] = []
+let scoreLevel: [String] = ["A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"]
 var isExecute = true
+
+// 인풋값이 숫자, 문자인지 확인하는 함수
+func validateInput(_ value: String) -> Bool{
+    let pattern = "^[0-9a-zA-Z]*$"
+    let predicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+    let isValid = predicate.evaluate(with: value)
+    return isValid
+}
 
 MAIN: while isExecute {
     print("원하는 기능을 입력해주세요")
@@ -10,7 +19,26 @@ MAIN: while isExecute {
 
     switch menu {
     case "1": // 학생 추가 기능
-        print("_")
+        while true {
+            print("추가할 학생의 이름을 입력해주세요")
+            let studentName = readLine()!
+
+            guard validateInput(studentName) else {
+                print("입력이 잘못되었습니다. 다시 확인해주세요")
+                continue MAIN
+            }
+
+            for item in list {
+                if item["name"] == studentName {
+                    print("\(studentName) 학생은 이미 존재합니다. 추가하지 않습니다")
+                    continue MAIN
+                }
+            }
+
+            list.append(["name": studentName])
+            print("\(studentName) 학생을 추가하였습니다")
+            continue MAIN
+        }
 
     case "2": // 학생 삭제 기능
         print("_")
