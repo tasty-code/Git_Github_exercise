abfa39e93695fdf0e01454d7e65647ff2089a2a4
devxsby <devxsby@gmail.com>
2022-12-19 11:13:02 +0900

    feat: add score enum types

---
+++ Git_ Exercise/enum/Score.swift
@@ -0,0 +1,20 @@
+//
+//  Score.swift
+//  Git_ Exercise
+//
+//  Created by devxsby on 2022/12/19.
+//
+
+import Foundation
+
+enum Score: String {
+    case aPlus = "A+"
+    case aZero = "A0"
+    case bPlus = "B+"
+    case bZero = "B0"
+    case cPlus = "C+"
+    case cZero = "C0"
+    case dPlus = "D+"
+    case dZero = "D0"
+    case fail = "F"
+}
