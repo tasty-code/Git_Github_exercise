5fd3cd9ebd2c01c664f7bcc850481b81f24838f7
Sei Kim <vanism7557@gmail.com>
2022-12-19 11:10:12 +0900

    feat: IOManager - 사용자 입력 받기 기능 추가

---
+++ Git_ Exercise/IOManager.swift
@@ -0,0 +1,29 @@
+//
+//  IOManager.swift
+//  Git_ Exercise
+//
+//  Created by sei_dev on 12/19/22.
+//
+
+import Foundation
+
+/// Console output type
+enum OutputType {
+    case information
+    case reaction
+    case error
+}
+
+enum IOManager {
+    static let nameRegex = "^[A-Za-z0-9_]+$"
+    
+    /// 사용자 입력 받기
+    static func getInput(isStartStatus: Bool) throws -> String {
+        guard let input = readLine(),
+              input != "" else {
+            throw isStartStatus ? CMError.invalidStartInput : IOError.wrongInput
+        }
+        return input
+    }
+    
+}
