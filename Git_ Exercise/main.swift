3781b6e7f8ab84ef135dab93c7365e881308f8e9
esther <wjswls109@naver.com>
2022-12-19 11:30:30 +0900

    Feat. 메뉴 기능 추가
    메뉴 1~5, X 버튼 입력 기능 추가

--- Git_ Exercise/main.swift
+++ Git_ Exercise/main.swift
@@ -1,11 +1,30 @@
-//
-//  main.swift
-//  Git_ Exercise
-//
-//  Created by 잼킹 on 2022/12/18.
-//
-
 import Foundation
 
-print("Hello, World!")
+var list: [Dictionary<String, String>] = []
+var isExecute = true
 
+MAIN: while isExecute {
+    print("원하는 기능을 입력해주세요")
+    print("1: 학생 추가, 2: 학생 삭제, 3: 성적 추가(변경), 4: 성적 삭제, 5: 평점보기, X: 종료 ")
+    let menu = readLine()!
+
+    switch menu {
+    case "1": // 학생 추가 기능
+        print("_")
+
+    case "2": // 학생 삭제 기능
+        print("_")
+    case "3": // 성적 추가(변경) 기능
+        print("_")
+
+    case "4": // 성적 삭제 기능
+        print("_")
+
+    case "5": // 평점보기 기능
+        print("_")
+    case "X", "x": // 종료 기능
+        print("_")
+    default:
+        print("잘못 입력하셨습니다. 1~5 사이의 숫자 또는 X를 입력해주세요")
+    }
+}
